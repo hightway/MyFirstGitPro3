2e72a5787d444c3cf6ac9d24f9ed04b6e9d7cfb8
Your Name <you@example.com>
2018-01-31 15:12:31 +0800

    新添加class

---
+++ app/src/main/java/com/intelcent/myfirstgitpro/myNewClassActivity.java
@@ -0,0 +1,9 @@
+package com.intelcent.myfirstgitpro;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by zhao on 2018/1/31.
+ */
+public class myNewClassActivity extends FragmentActivity{
+}
