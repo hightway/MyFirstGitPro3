1f3b68824bb10a9d877cacc97d0395bdb73ba922
Your Name <you@example.com>
2018-01-31 16:11:16 +0800

    新添加class

--- app/src/main/java/com/intelcent/myfirstgitpro/myNewClassActivity.java
+++ app/src/main/java/com/intelcent/myfirstgitpro/myNewClassActivity.java
@@ -1,9 +1,47 @@
 package com.intelcent.myfirstgitpro;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 
 /**
  * Created by zhao on 2018/1/31.
  */
 public class myNewClassActivity extends FragmentActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 }
+
